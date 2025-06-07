e05aee39240239f4fb9fdf2a775be0939472c363
kevinseya <kevinseya@gmail.com>
2025-06-07 17:35:49 -0500

    perf: :white_check_mark: Create a footer and main butom of page home

--- App.js
+++ App.js
@@ -44,14 +44,23 @@ export default function App() {
           style={styles.logo}
         />
       </View>
-      
+      {/* Círculo divisor*/}
+      <View style={styles.circleContainer}>
+        <View style={styles.circle}>
+        </View>
+      </View>
+
       {/* Cuerpo de la pantalla con contenido */}
       <View style={styles.body}>
         <View style={styles.content}>
           <Text style={styles.text}>Últimas noticias en la UCE..</Text>
+          <View style={styles.card_social_network}>
+          </View>
         </View>
       </View>
-      
+      <View style={styles.footer}>
+       
+      </View>
       <StatusBar style="light" />
     </View>
   );
@@ -100,5 +109,40 @@ const styles = StyleSheet.create({
     color: '#AD9219',
     fontFamily: 'Montserrat-Regular',
     textAlign: 'center',
+  },
+  card_social_network:{
+    backgroundColor: 'rgba(128, 129, 131, 0.35)',
+    opacity: 0.35,
+    width: 350,
+    height: 130,
+    borderRadius: 20, 
+  },
+  footer: {
+    backgroundColor: '#808183', 
+    height: 80,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  circleContainer: {
+    position: 'absolute',
+    bottom: 30, 
+    left: 0,
+    right: 0,
+    alignItems: 'center',
+    zIndex: 2, 
+  },
+    circle: {
+    width: 90,
+    height: 90,
+    borderRadius: 50, 
+    backgroundColor: '#808183', 
+    borderWidth: 7,
+    borderColor: 'white', 
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
